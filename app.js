e92ab0b6243493c87a4760c74f2a29ff4d84b881
tutyamxx <tutygoo@gmail.com>
2020-01-15 19:19:17 +0000

    Some updates

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const jokesRouter = require('./routes/romanianjoke');
+const jokesRouter = require('./routes/api');
 
 let app = express();
 
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/romanianjoke', jokesRouter);
+app.use('/api', jokesRouter);
 
 // --| Catch 404 and forward to error handler
 app.use(function(req, res, next)
@@ -31,7 +31,11 @@ app.use(function(err, req, res, next)
 
     // --| Render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.json(
+    {
+        message: err.message,
+        error: err
+    });
 });
 
 module.exports = app;
--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+router.use((req, res, next) =>
+{
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 const RomanianJokesFile = require("../jokes/romanianjokes.json");
 
+router.get("/", (req, res, next) =>
+{
+    return res.status(200).json({ message: "Try /api/romanianjokes or /api/romanianjokes/seci/ for example" });
+});
+
 // --| Get a random Romanian joke from the database and return it as JSON response
-router.get("/", function(req, res, next)
+router.get("/romanianjokes", (req, res, next) =>
 {
     // --| Get a random romanian joke from our JSON file
     const GetRandomJoke = RomanianJokesFile[Math.floor(Math.random() * RomanianJokesFile.length)];
 
     // --| Return the joke as JSON result
-    res.status(200).json(GetRandomJoke);
+    return res.status(200).json(GetRandomJoke);
 });
 
 module.exports = router;
